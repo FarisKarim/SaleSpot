5ad01f3979a21fab54affad9a1ee328a870060ee
Faris <fariskarim2000@gmail.com>
2024-01-31 22:03:13 -0500

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,13 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: 'SaleSpot',
+}
+
 
 export default function RootLayout({ children }) {
   return (
